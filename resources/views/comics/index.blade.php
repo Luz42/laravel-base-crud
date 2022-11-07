db1e76982f4f3eb298bd32da94fbd7703593b18a
Luz42 <a.evangelista93@gmail.com>
2022-11-07 15:47:21 +0100

    Creato un layout ed esteso per entrambe le pagine

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -1,17 +1,14 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Comics</title>
-</head>
-<body>
+@extends('layouts.layout')
+
+@section('title')
+    Comics
+@endsection
+
+@section('content')
     @foreach ($comics as $comic)
         <div>
             <h4>{{$comic['title']}}</h4>
             <a href="{{ route('comics.show', $comic->id) }}">Dettagli</a>
         </div>
     @endforeach
-</body>
-</html>
+@endsection
--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -1,22 +1,17 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <ul>
+@extends('layouts.layout')
 
-            <li><h4>{{$comic['title']}}</h4></li>
-            <li><p>{{$comic['description']}}</p></li>
-            <li><img src="{{$comic['thumb']}}" alt="Immagine di:{{$comic['title']}}"></li>
-            <li><p>{{$comic['price']}}</p></li>
-            <li><p>{{$comic['series']}}</p></li>
-            <li><p>{{$comic['sale_date']}}</p></li>
-            <li><p>{{$comic['type']}}</p></li>
+@section('title')
+    {{$comic['title']}}
+@endsection
 
-    </ul>
-</body>
-</html>
+@section('content')
+<ul>
+    <li><h4>{{$comic['title']}}</h4></li>
+    <li><p>{{$comic['description']}}</p></li>
+    <li><img src="{{$comic['thumb']}}" alt="Immagine di:{{$comic['title']}}"></li>
+    <li><p>{{$comic['price']}}</p></li>
+    <li><p>{{$comic['series']}}</p></li>
+    <li><p>{{$comic['sale_date']}}</p></li>
+    <li><p>{{$comic['type']}}</p></li>
+</ul>    
+@endsection
