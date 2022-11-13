eee8e092a49e9f889b9143f394e4a15511c72c6b
Luz42 <a.evangelista93@gmail.com>
2022-11-13 14:12:39 +0100

    Validazione lato frontend del form in comics.edit
    implementata la gestione degli errrori

--- resources/views/comics/edit.blade.php
+++ resources/views/comics/edit.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.layout')
 
 @section('title')
-    Update {{$comic->title}}
+    {{__('Update').' '. $comic->title}}
 @endsection
 
 @section('content')
@@ -10,43 +10,64 @@
     @csrf
     @method('PUT')
     <div>
-        <label for="title">Titolo:</label>
-        <input type="text" name="title" required maxlength="50" value="{{$comic->title}}">
+        <label for="title">{{__('Titolo:')}}</label>
+        <input @error('title') class="is-invalid" @enderror type="text" name="title"  maxlength="50" value="{{old('title', $comic->title)}}" required>
+        @error('title')
+            <span>{{$message}}</span>
+        @enderror
     </div>
 
     <div>
-        <label for="description">Descrizione:</label>
-        <input type="text" name="description" cols="20" rows="10" required value="{{$comic->description}}"></input type="text">
+        <label for="description">{{__('Descrizione:')}}</label>
+        <textarea @error('description') class="is-invalid" @enderror name="description" cols="20" rows="1" required>{{old('description', $comic->description)}}</textarea>
+        @error('description')
+            <span>{{$message}}</span>
+        @enderror
     </div>
 
     <div>
-        <label for="thumb">Url dell'immagine:</label>
-        <input type="url" pattern="https://.*" name="thumb" required value="{{$comic->thumb}}">
+        <label for="thumb">{{__('Url dell\'immagine:')}}</label>
+        <input @error('thumb') class="is-invalid" @enderror type="url" pattern="https://.*" name="thumb" value="{{old('thumb', $comic->thumb)}}" required>
+        @error('thumb')
+            <span>{{$message}}</span>
+        @enderror
     </div>
 
     <div>
-        <label for="price">Prezzo:</label>
-        <input type="number" name='price' min='0' step='.01' max='9999.99' required value="{{$comic->price}}">
+        <label for="price">{{__('Prezzo:')}}</label>
+        <input @error('price') class="is-invalid" @enderror type="number" name='price' min='0' step='.01' max='9999.99' value="{{old('price', $comic->price)}}" required>
+        @error('price')
+            <span>{{$message}}</span>
+        @enderror
     </div>
 
     <div>
-        <label for="series">Serie:</label>
-        <input type="text" name="series" required maxlength="50" value="{{$comic->series}}">
+        <label for="series">{{__('Serie:')}}</label>
+        <input @error('series') class="is-invalid" @enderror type="text" name="series"  maxlength="50" value="{{old('series', $comic->series)}}" required>
+        @error('series')
+            <span>{{$message}}</span>
+        @enderror
     </div>
 
     <div>
-        <label for="sale_date">Data della vendita:</label>
-        <input type="date" name="sale_date" required value="{{$comic->date}}">
+        <label for="sale_date">{{__('Data della vendita:')}}</label>
+        <input @error('sale_date') class="is-invalid" @enderror type="date" name="sale_date" value="{{old('sale_date', $comic->sale_date)}}" required>
+        @error('sale_date')
+            <span>{{$message}}</span>
+        @enderror
     </div>
 
     <div>
-        <label for="type">Genere:</label>
-        <input type="text" name="type" required maxlength="20" value="{{$comic->type}}">
+        <label for="type">{{__('Genere:')}}</label>
+        <input @error('type') class="is-invalid" @enderror type="text" name="type"  maxlength="20" value="{{old('type', $comic->type)}}" required>
+        @error('type')
+            <span>{{$message}}</span>
+        @enderror
     </div>
 
-    <input type="submit" value="Aggiorna">
+    <input type="submit" value="{{__('Aggiorna')}}">
 
 </form>
 
-<a href="{{route('comics.show', $comic->id)}}">Annulla</a>
+<a href="{{route('comics.show', $comic->id)}}">{{__('Annulla')}}</a>
 @endsection
