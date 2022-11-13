ab25942746fda5b351434e62ab1edf82d7903ea3
Luz42 <a.evangelista93@gmail.com>
2022-11-13 13:57:50 +0100

    Aggiunta la funzione per un eventuale traduttore

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -8,7 +8,7 @@
     <form action="{{route('comics.store')}}" method="post">
         @csrf
         <div>
-            <label for="title">Titolo:</label>
+            <label for="title">{{__('Titolo:')}}</label>
             <input @error('title') class="is-invalid" @enderror type="text" name="title"  maxlength="50" value="{{old('title', '')}}" required>
             @error('title')
                 <span>{{$message}}</span>
@@ -16,7 +16,7 @@
         </div>
 
         <div>
-            <label for="description">Descrizione:</label>
+            <label for="description">{{__('Descrizione:')}}</label>
             <textarea @error('description') class="is-invalid" @enderror name="description" cols="20" rows="1" required>{{old('description', '')}}</textarea>
             @error('description')
                 <span>{{$message}}</span>
@@ -24,7 +24,7 @@
         </div>
 
         <div>
-            <label for="thumb">Url dell'immagine:</label>
+            <label for="thumb">{{__('Url dell\'immagine:')}}</label>
             <input @error('thumb') class="is-invalid" @enderror type="url" pattern="https://.*" name="thumb" value="{{old('thumb', '')}}" required>
             @error('thumb')
                 <span>{{$message}}</span>
@@ -32,7 +32,7 @@
         </div>
 
         <div>
-            <label for="price">Prezzo:</label>
+            <label for="price">{{__('Prezzo:')}}</label>
             <input @error('price') class="is-invalid" @enderror type="number" name='price' min='0' step='.01' max='9999.99' value="{{old('price', '')}}" required>
             @error('price')
                 <span>{{$message}}</span>
@@ -40,7 +40,7 @@
         </div>
 
         <div>
-            <label for="series">Serie:</label>
+            <label for="series">{{__('Serie:')}}</label>
             <input @error('series') class="is-invalid" @enderror type="text" name="series"  maxlength="50" value="{{old('series', '')}}" required>
             @error('series')
                 <span>{{$message}}</span>
@@ -48,7 +48,7 @@
         </div>
 
         <div>
-            <label for="sale_date">Data della vendita:</label>
+            <label for="sale_date">{{__('Data della vendita:')}}</label>
             <input @error('sale_date') class="is-invalid" @enderror type="date" name="sale_date" value="{{old('sale_date', '')}}" required>
             @error('sale_date')
                 <span>{{$message}}</span>
@@ -56,7 +56,7 @@
         </div>
 
         <div>
-            <label for="type">Genere:</label>
+            <label for="type">{{__('Genere:')}}</label>
             <input @error('type') class="is-invalid" @enderror type="text" name="type"  maxlength="20" value="{{old('type', '')}}" required>
             @error('type')
                 <span>{{$message}}</span>
